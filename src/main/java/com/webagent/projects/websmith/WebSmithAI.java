1725377469c55c8ed1a50466a7ac56df1398e741
Dhruv Pathak <dhruvpathak414@gmail.com>
2026-02-22 13:27:14 +0530

    minor bug fix

--- src/main/java/com/webagent/projects/websmith/WebSmithAI.java
+++ src/main/java/com/webagent/projects/websmith/WebSmithAI.java
@@ -1,5 +1,6 @@
 package com.webagent.projects.websmith;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebSmithAI {
 
 	public static void main(String[] args) {
+		// Load .env file
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		
+		// Set environment variables from .env file
+		dotenv.entries().forEach(entry -> 
+			System.setProperty(entry.getKey(), entry.getValue())
+		);
+		
 		SpringApplication.run(WebSmithAI.class, args);
 	}
 
